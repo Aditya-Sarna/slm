935c580eefeac12a56f81524ef7f478509b8de82
Aditya-Sarna <107273722+Aditya-Sarna@users.noreply.github.com>
2026-01-31 00:57:24 +0530

    Add tokenization for BERT and GPT-2 models

--- tokeniser.py
+++ tokeniser.py
@@ -1,1 +1,22 @@
 
+from transformers import BertTokenizerFast
+
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
+
+text =str(input("Enter text to tokenize: "))
+tokens = tokenizer.tokenize(text)
+ids = tokenizer.encode(text)
+
+print("Tokens:", tokens)
+print("Token IDs:", ids)
+
+from transformers import GPT2TokenizerFast
+
+tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+
+text = str(input("Enter text to tokenize: "))
+tokens = tokenizer.tokenize(text)
+ids = tokenizer.encode(text)
+
+print("Tokens:", tokens)
+print("Token IDs:", ids)
